e8d81e7218e9c080e08f03f46e68dc3cda4321a4
Amaan <amaanmas1@gmail.com>
2023-01-20 18:52:47 +0530

    Added Items

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,8 +1,12 @@
 import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
 
 const Meals = () => {
     return(
-        <MealsSummary />
+        <>
+            <MealsSummary />
+            <AvailableMeals />
+        </>
     )
 }
 
