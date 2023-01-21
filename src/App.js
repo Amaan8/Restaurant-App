a90fd792de5ae6521a6d1efeb5f92dce172d6895
Amaan <amaanmas1@gmail.com>
2023-01-21 16:17:13 +0530

    Added state on Cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import {useState} from 'react';
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cart, setCart] = useState(false);
+
+  const showCart = () => {
+    setCart(true);
+  }
+
+  const hideCart = () => {
+    setCart(false);
+  }
+
   return (
     <>
-      <Cart />
-      <Header />
+      {cart && <Cart onClose={hideCart} />}
+      <Header onShowCart={showCart} />
       <main>
         <Meals />
       </main>
